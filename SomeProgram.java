0dfdbbd86d15e6e9fda8c10c484e985fdc313eb0
Lukasz Biernacki <lbiernacki@student.agh.edu.pl>
2024-01-20 15:07:48 +0100

    New method manageTheSatelite was added

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,4 +1,9 @@
-public class SomeProgram { 
+public class SomeProgram {
+	
+	public void manageTheSatellite() {
+    // TODO implement
+}
+	
     public static void main(String[] args) {
       System.out.println("We are learning to use Git."); 
 	  System.out.println("We will be Git experts, soon.");
